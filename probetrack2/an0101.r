0f21ac32f371cf1ea5a341ea65389ab1aaae2170
David Fencsik <david.fencsik@gmail.com>
2006-07-06 17:04:17 +0000

    probetrack2/:
    * 00analyses.txt, refresh.r, data01.r, an0102.r, an0103.r, an0104.r:
      copied unmodified from ../probetrack1/
    
    * an0101.r: copied from ../probetrack1/ and modified because a different
      subject is missing data from a no-gap trial
    
    * fig0101.r, fig0102.r: copied from ../probetrack1/ and changed title

---
+++ probetrack2/an0101.r
@@ -0,0 +1,50 @@
+### an0101: anova on RT as a function of probe delay and probe type
+###
+### $LastChangedDate$
+
+do.an0101 <- function () {
+   thisfile <- "an0101.r";
+   infile <- "data01.rda";
+   outfile <- "an0101.txt";
+
+   exit.function <- function () {
+      while (sink.number() > 0) sink();
+   }
+   on.exit(exit.function());
+
+   if (!file.exists(infile)) stop("cannot open input file ", infile);
+   if (file.exists(outfile) &&
+       file.info(outfile)$mtime > file.info(thisfile)$mtime &&
+       file.info(outfile)$mtime > file.info(infile)$mtime) {
+      warning("Output file is up to date, no action taken");
+      invisible(NULL);
+   }
+   load(infile);
+
+   sink(outfile);
+   cat("ANOVA on correct RT as a function of probe type and probe delay\n");
+   cat("  gap trials only\n");
+   print(summary(aov(rt.cor ~ target * soa + Error(sub / (target * soa)),
+                     data01[data01$gapdur == "10",])));
+
+   cat("\n\n\n");
+   cat("ANOVA on all RT as a function of probe type and probe delay\n");
+   cat("  gap trials only\n");
+   print(summary(aov(rt.all ~ target * soa + Error(sub / (target * soa)),
+                     data01[data01$gapdur == "10",])));
+
+   cat("\n\n\n");
+   cat("ANOVA on correct RT as a function of probe type and probe delay\n");
+   cat("  no-gap trials only, subject with missing data eliminated\n");
+   print(summary(aov(rt.cor ~ target * soa + Error(sub / (target * soa)),
+                     data01[data01$gapdur == "0" & data01$sub != "dt",])));
+
+   cat("\n\n\n");
+   cat("ANOVA on all RT as a function of probe type and probe delay\n");
+   cat("  no-gap trials only\n");
+   print(summary(aov(rt.all ~ target * soa + Error(sub / (target * soa)),
+                     data01[data01$gapdur == "0",])));
+}
+
+do.an0101();
+rm(do.an0101);
---
+++ probetrack2/fig0102.r
@@ -0,0 +1,76 @@
+### fig0102.r: plot accuracy by probe delay separated by probe type
+###
+### $LastChangedDate$
+
+do.fig0102 <- function () {
+   infile <- "data01.rda";
+   outfile <- "fig0102.pdf";
+   thisfile <- "fig0102.r";
+   exit.function <- function () {
+      if (exists("opar")) par(opar);
+      if (any(names(dev.cur()) == c("postscript", "pdf"))) dev.off();
+   }
+   on.exit(exit.function());
+
+   if (!file.exists(infile)) stop("cannot open input file ", infile);
+   if (file.exists(outfile) &&
+       file.info(outfile)$mtime > file.info(thisfile)$mtime &&
+       file.info(outfile)$mtime > file.info(infile)$mtime) {
+      warning("Output file is up to date, no action taken");
+      return(NULL);
+   }
+   load(infile);
+   data01$soa <- as.numeric(as.character(data01$soa));
+
+   ## extract relevant data
+   dtg <- with(data01[data01$gapdur != "0", ],
+               tapply(pcor, list(soa, target), mean, na.rm = TRUE));
+   errg <- with(data01[data01$gapdur != "0", ],
+                tapply(pcor, list(soa, target),
+                       function(x) sqrt(var(x, na.rm = TRUE) / length(x))));
+   dtng <- with(data01[data01$gapdur == "0", ],
+                tapply(pcor, list(soa, target), mean, na.rm = TRUE));
+
+   x <- as.numeric(dimnames(dtg)[[1]]) * 1000 / 75;
+
+   ## settings
+   ylim <- c(.5, 1);
+   cond.names <- dimnames(dtg)[[2]];
+   nCond <- length(cond.names);
+   col <- rainbow(nCond);                               names(col) <- cond.names;
+   pch <- c(21, 22, 23, 24, 16, 15, 17, 18)[1:nCond];   names(pch) <- cond.names;
+
+   pdf(outfile, width = 6, height = 6, pointsize = 12);
+   opar <- par(mfrow = c(1, 1), las = 1, pty = "m", cex.axis = .6,
+               xpd = NA, bg = "white");
+
+   matplot(x, dtg, type = "n", bty = "n",
+           ylim = ylim, axes = F,
+           xlab = "Probe delay (ms)", ylab = "Proportion of correct trials", main = "ProbeTrack2");
+   axis(1, x);
+   axis(2);
+
+   lastIndex <- dim(dtg)[1];
+   for (targ in dimnames(dtg)[[2]]) {
+      if (!is.null(errg)) {
+         arrows(x, dtg[, targ] - errg[, targ], x, dtg[, targ] + errg[, targ],
+                length = .05, angle = 90, code = 3, lwd = 1, col = col[targ], lty = 1);
+      }
+      lines(x, dtg[, targ], type = "o",
+            col = col[targ], pch = pch[targ], lty = 1, lwd = 3, cex = 1.5, bg = "white");
+      lines(range(x), rep(mean(dtng[, targ]), 2), type = "l",
+            col = col[targ], lty = 2, lwd = 3);
+###       text(x[lastIndex] + xinch(.2), dtg[lastIndex, n, targ], sprintf("%s targets", n),
+###            col = col[targ], cex = .7, adj = 0);
+      legend(max(x) - xinch(2), max(ylim) + yinch(.25), c("probe distractor", "probe target"),
+             col = col[as.character(0:1)], pch = pch[as.character(0:1)],
+             lty = 1, lwd = 3, pt.bg = "white", pt.cex = 1.5,
+             bty = "n", y.intersp = 1.3);
+      legend(max(x) - xinch(3.5), max(ylim) + yinch(.25), c("gap", "no gap"),
+             lty = 1:2, lwd = 3,
+             bty = "n", y.intersp = 1.3);
+   }
+}
+
+do.fig0102();
+rm(do.fig0102);
