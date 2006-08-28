ca65b88bba0a8fdb667bfeafe3463c8b753d1ff3
David Fencsik <david.fencsik@gmail.com>
2006-08-28 20:25:59 +0000

    probetrack1/:
    * BUG IN THIS COMMIT: d' values produced by data02 seem to be
      incorrect, since there should be a difference between gap and no-gap
      trials
    
    * 00analyses.txt: marked data02 family of analyses as complete, and
      added fig0201 to the group (also complete)
    
    * data02.r:
      - initial commit
      - non-svn copied from ~/crossover/satsearch01/data02.r and modified
        for this project
      - keywords set to date
      - CURRENT VERSION IS BUGGY; SEE ABOVE
    
    * an0201.r, an0202.r: copied from an0302.r and an0304.r and modified
      to perform analyses on d' instead of accuracy
    
    * fig0201.r: copied from fig0302.r and modified to plot d'

---
+++ probetrack1/an0201.r
@@ -0,0 +1,36 @@
+### an0201: anova on d' as a function of probe delay
+###
+### $LastChangedDate$
+
+do.an0201 <- function () {
+   thisfile <- "an0201.r";
+   infile <- "data02.rda";
+   outfile <- "an0201.txt";
+
+   exit.function <- function () {
+      while (sink.number() > 0) sink();
+   }
+   on.exit(exit.function());
+
+   if (!file.exists(infile)) stop("cannot open input file ", infile);
+   if (IsFileUpToDate(outfile, c(thisfile, infile))) {
+      warning("Output file is up to date, no action taken");
+      return(invisible(NULL));
+   }
+   load(infile);
+
+   sink(outfile);
+   cat("ANOVA on d' as a function of probe delay\n");
+   cat("  gap trials only\n");
+   print(summary(aov(dprime ~ soa + Error(sub / soa),
+                     data02[data02$gapdur == "10",])));
+
+   cat("\n\n\n");
+   cat("ANOVA on d' as a function of probe delay\n");
+   cat("  no-gap trials only\n");
+   print(summary(aov(dprime ~ soa + Error(sub / soa),
+                     data02[data02$gapdur == "0",])));
+}
+
+do.an0201();
+rm(do.an0201);
