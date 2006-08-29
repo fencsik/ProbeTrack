de633bb087dee39187caed90b08cefbd03261ad7
David Fencsik <david.fencsik@gmail.com>
2006-08-29 18:47:37 +0000

    probetrack2/:
    * 00analyses.txt: updated to latest version from ../probetrack1/ using
    
        svn merge -r 947:HEAD ../probetrack1/00analyses.txt 00analyses.txt
    
      adding in section dividers, marking data02 family as complete, and
      adding summaries of complete data03 family
    
    * data01.r: updated to latest version from ../probetrack1/
    
        svn merge -r 948:949 ../probetrack1/data01.r data01.r
    
      added computation of ncor (# correct)
    
    * fig0201.r, fig0301.r, fig0302.r: copied from ../probetrack1/ and
      modified title for ProbeTrack2
    
    * data02.r, data03.r, an0201.r, an0202.r, an0301.r, an0302.r,
      an0303.r, an0304.r: copied from ../probetrack1/ without
      modifications

---
+++ probetrack2/an0304.r
@@ -0,0 +1,52 @@
+### an0304: t-tests comparing accuracy on gap trials at each probe delay to
+### accuracy on no-gap trials
+###
+### $LastChangedDate$
+
+do.an0304 <- function () {
+   thisfile <- "an0304.r";
+   infile <- "data03.rda";
+   outfile <- "an0304.txt";
+   rdafile <- "an0304.rda";
+
+   exit.function <- function () {
+      while (sink.number() > 0) sink();
+   }
+   on.exit(exit.function());
+
+   if (!file.exists(infile)) stop("cannot open input file ", infile);
+   if (IsFileUpToDate(outfile, c(thisfile, infile))) {
+      warning("Output file is up to date, no action taken");
+      return(invisible(NULL));
+   }
+   load(infile);
+   data03$soa <- as.numeric(as.character(data03$soa)) * 1000 / 75;
+
+   d.gap <- with(data03[data03$gapdur == "10",],
+                 tapply(pcor, list(sub, soa), mean));
+   d.nogap <- with(data03[data03$gapdur == "0",],
+                   tapply(pcor, list(sub), mean));
+
+   results <- array(dim = c(dim(d.gap)[2], 3),
+                    dimnames = list(dimnames(d.gap)[[2]], c("t", "p", "ci")));
+
+   for (soa in dimnames(d.gap)[[2]]) {
+      g <- t.test(d.gap[, soa], d.nogap, paired = T, var.equal = T);
+      results[soa, "t"] <- round(g$statistic, 2);
+      results[soa, "p"] <- round(g$p.value, 3);
+      results[soa, "ci"] <- diff(as.numeric(g$conf.int)) / 2;
+   }
+
+   sink(outfile);
+   cat(c("Paired t-tests comparing accuracy between gap and no-gap trials",
+         "at each probe delay.  The first column lists the t-statistics, the",
+         "second the corresponding p-values, and the third 95% CIs.  Rows",
+         "indicate probe delay.\n"), sep = "\n");
+   print(results);
+
+   an0304 <- results;
+   save(an0304, file = rdafile);
+}
+
+do.an0304();
+rm(do.an0304);
---
+++ probetrack2/data03.r
@@ -0,0 +1,51 @@
+### data03
+###
+### $LastChangedDate$
+
+do.data03 <- function () {
+   infile <- "rawdata.txt";
+   outfile <- "data03.rda";
+   thisfile <- "data03.r";
+
+   if (!file.exists(infile)) stop("cannot open file ", infile);
+   if (IsFileUpToDate(outfile, c(thisfile, infile))) {
+      warning("Output file is up to date, no action taken");
+      return(invisible(NULL));
+   }
+   dt <- read.csv(infile);
+
+   ## remove bad subjects:
+   ## 1. remove nw based on SSP's lab book
+   dt <- dt[dt$identifier != "nw", ];
+   dt$sub <- factor(dt$identifier);
+
+   ## remove practice blocks, bad keypresses, and negative RTs (which should
+   ## just indicate bad key presses)
+   dt <- dt[dt$block == 2 & dt$wrongKeyFlag == 0 & !is.na(dt$RT) & dt$RT > 0, ];
+
+   ## rename and recode variables
+   dt$acc <- 1 - dt$error;
+   dt$rt <- dt$RT;
+
+   factors <- list(sub = dt$sub, gapdur = dt$gapDuration, ntargets = dt$nTargets, soa = dt$SOA);
+   data03 <- aggregate(dt$acc, factors, length);
+   names(data03)[names(data03) == "x"] <- "nobs";
+   data03$pcor <- aggregate(dt$acc, factors, mean)$x;
+
+   rt.cor <- rt.all <- rep(NA, dim(data03)[1]);
+   for (i in 1:dim(data03)[1]) {
+      index <- (dt$sub == data03$sub[i] & dt$gapDuration == data03$gapdur[i] &
+                dt$nTargets == data03$ntargets[i] & dt$SOA == data03$soa[i]);
+      if (any(index)) rt.all[i] <- mean(dt[index, "RT"]);
+      index <- index & dt$acc == 1;
+      if (any(index)) rt.cor[i] <- mean(dt[index, "RT"]);
+   }
+   data03$rt.all <- rt.all;
+   data03$rt.cor <- rt.cor;
+
+   save(data03, file=outfile)
+   invisible(data03)
+}
+
+print(system.time(do.data03()));
+rm(do.data03);
