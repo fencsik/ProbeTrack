2b0b0a7e1db76eddd7560796c76d1f4432c3b87f
David Fencsik <david.fencsik@gmail.com>
2007-03-07 15:18:03 +0000

    probetrack1/:
    * 00analyses.txt: modified data05 summary, marked it as complete, and
      added summaries for data05-based analyses analogous to those of the
      data04-based analyses
    
    * data05.r: copied from data04.r and modified to used data01 data
      file, removing distractor-probe trials
    
    * an0501.r, an0502.r, fig0501.r, fig0502.r: copied from analogous
      data04-based analyses and modified to used data05 instead
    
    * fig0404.r: allow for flexible subject selection, so we pick the same
      subjects for the x- and y-axes
    
    * fig0504.r: copied from fig0404.r, modified to plot based on data05
      data, and made change described above for fig0404.r

--- probetrack1/fig0504.r
+++ probetrack1/fig0504.r
@@ -1,13 +1,13 @@
-### fig0404.r: scatter-plot of weibull baseline against zero-gap RT for each
+### fig0504.r: scatter-plot of weibull baseline against zero-gap RT for each
 ### subject
 ###
 ### $LastChangedDate$
 
-do.fig0404 <- function () {
-   infile.baseline <- "data04.rda";
-   infile.control <- "data03.rda";
-   outfile <- "fig0404.pdf";
-   thisfile <- "fig0404.r";
+do.fig0504 <- function () {
+   infile.baseline <- "data05.rda";
+   infile.control <- "data01.rda";
+   outfile <- "fig0504.pdf";
+   thisfile <- "fig0504.r";
    exit.function <- function () {
       if (exists("opar")) par(opar);
       if (any(names(dev.cur()) == c("postscript", "pdf"))) dev.off();
@@ -19,12 +19,13 @@ do.fig0404 <- function () {
       return(invisible(NULL));
    }
    load(infile.control);
-   data03$gapdur <- as.numeric(as.character(data03$gapdur));
-   data03$ntargets <- as.numeric(as.character(data03$ntargets));
-   control <- with(data03[data03$gapdur == "0", ], tapply(rt.cor, list(sub, ntargets), mean));
+   data01$gapdur <- as.numeric(as.character(data01$gapdur));
+   data01$ntargets <- as.numeric(as.character(data01$ntargets));
+   control <- with(data01[data01$gapdur == "0" & data01$target == "1", ],
+                   tapply(rt.cor, list(sub, ntargets), mean));
 
    load(infile.baseline);
-   baseline <- with(data04$fit, tapply(baseline, list(sub, gapdur, ntargets), mean));
+   baseline <- with(data05$fit, tapply(baseline, list(sub, gapdur, ntargets), mean));
    Subjects <- dimnames(baseline)[[1]];
 
    ylim <- c(400, 1000);
@@ -43,5 +44,5 @@ do.fig0404 <- function () {
    }
 }
 
-do.fig0404();
-rm(do.fig0404);
+do.fig0504();
+rm(do.fig0504);
