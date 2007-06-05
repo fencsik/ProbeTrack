ebae3f37eee5ca585f08cc3a5c8d57802a5fabb6
David Fencsik <david.fencsik@gmail.com>
2007-06-05 20:47:39 +0000

    probetrackpaper/an/:
    * started adding in scripts for data10 analyses, with data10.r and
      an1001.r complete, and 00analyses.txt summaries for all data10 and
      data11 analyses copied from ../probetrack1/00analyses.txt

---
+++ probetrack3/an1001.r
@@ -0,0 +1,46 @@
+### an1001: anova on median correct RT as a function of probe delay and gap
+### duration
+###
+### $LastChangedDate$
+
+do.an1001 <- function () {
+   thisfile <- "an1001.r";
+   infile <- "data10.rda";
+   outfile <- "an1001.txt";
+
+   exit.function <- function () {
+      while (sink.number() > 0) sink();
+   }
+   on.exit(exit.function());
+
+   if (!file.exists(infile)) stop("cannot open input file ", infile);
+   if (IsFileUpToDate(outfile, c(thisfile, infile))) {
+      warning("Output file is up to date, no action taken");
+      return(invisible(NULL));
+   }
+   load(infile);
+
+   sink(outfile);
+   cat("ANOVA on median correct RT as a function of probe delay and gap duration\n");
+   cat("  gap trials only\n");
+   print(summary(aov(rt ~ soa * gapdur + Error(sub / (soa * gapdur)),
+                     data10[data10$gapdur != "0",])));
+
+   for (gd in levels(data10$gapdur)) {
+      if (gd == "0") next;
+      cat("\n\n\n");
+      cat(sprintf("ANOVA on RT as a function of probe delay at gap duration %s\n", gd));
+      print(summary(aov(rt ~ soa + Error(sub / (soa)),
+                        data10[data10$gapdur == gd,])));
+   }
+
+   for (s in as.character(sort(as.numeric(levels(data10$soa))))) {
+      cat("\n\n\n");
+      cat(sprintf("ANOVA on RT as a function of gap duration at SOA = %s\n", s));
+      print(summary(aov(rt ~ gapdur + Error(sub / (gapdur)),
+                        data10[data10$gapdur != "0" & data10$soa == s,])));
+   }
+}
+
+do.an1001();
+rm(do.an1001);
---
+++ probetrack3/data10.r
@@ -0,0 +1,46 @@
+### data10
+###
+### $LastChangedDate$
+
+do.data10 <- function () {
+   infile <- "rawdata.txt";
+   outfile <- "data10.rda";
+   thisfile <- "data10.r";
+
+   if (!file.exists(infile)) stop("cannot open file ", infile);
+   if (IsFileUpToDate(outfile, c(thisfile, infile))) {
+      warning("Output file is up to date, no action taken");
+      return(invisible(NULL));
+   }
+   dt <- read.csv(infile);
+
+   ## remove bad subjects:
+   ## 1. remove tjo based on SSP's lab book (not listed in the lab book)
+   ## 2. optionally remove jtd and rsh  because the weibull fits are weird
+   dt <- dt[dt$sub != "tjo", ];
+   ##dt <- dt[dt$sub != "tjo" & dt$sub != "jtd" & dt$sub != "rsh", ];
+   dt$sub <- factor(dt$sub);
+
+   ## remove practice blocks, bad keypresses, and negative RTs (which should
+   ## just indicate bad key presses)
+   dt <- dt[dt$block == 2 & dt$badkey == 0 & !is.na(dt$rt) & dt$rt > 0, ];
+
+   ## rename and recode variables
+   dt$acc <- 1 - dt$error;
+   dt$soa <- dt$soa * 1000 / 75; # convert to ms
+   dt$gapdur <- round(dt$gapdur * 1000 / 75);
+   dt$soa[dt$gapdur == 0] <- 0; # set SOA to 0 on no-gap trials
+
+   ## extract factors for all trials and for all correct trials
+   dt2 <- dt[dt$acc == 1, ];
+   factorsC <- with(dt2, list(soa=soa, ntargets=ntargets, gapdur=gapdur, sub=sub));
+
+   ## collapse across the factors
+   data10 <- aggregate(data.frame(rt = dt2$rt), factorsC, median);
+
+   save(data10, file=outfile)
+   invisible(data10)
+}
+
+print(system.time(do.data10()));
+rm(do.data10);
