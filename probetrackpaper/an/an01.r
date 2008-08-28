84a1a082de5ac8cea9976ea3559ed9067923aa0f
David Fencsik <david.fencsik@gmail.com>
2008-08-28 22:29:47 +0000

    propetrackpaper/an/:
    * an01.r: Added ProbeTrack6b to analysis.  The analysis was failing
      because it couldn't handle the subjects missing from the
      demographics file corrects, so I thoroughly revised the code so it's
      a little more robust and possibly more efficient.  Also updated it
      to modern R coding standards.

--- probetrackpaper/an/an01.r
+++ probetrackpaper/an/an01.r
@@ -2,96 +2,108 @@
 ###
 ### $Id$
 
-do.an01 <- function() {
-   infiles <- c("../../probetrack1/data01.rda",
-                "../../probetrack2/data01.rda",
-                "../../probetrack3/data01.rda");
-   demofile <- "demographics.txt";
-   outfile <- "an01out.txt"
+f.an01 <- function() {
+    infiles <- c("../../probetrack1/data01.rda",
+                 "../../probetrack2/data01.rda",
+                 "../../probetrack3/data01.rda",
+                 "../../probetrack6b/data01.rda"
+                 );
+    demofile <- "demographics.txt";
+    outfile <- "an01out.txt"
 
-   exit.function <- function () {
-      while (sink.number() > 0) sink();
-   }
-   on.exit(exit.function());
+    exit.function <- function () {
+        while (sink.number() > 0) sink()
+    }
+    on.exit(exit.function())
 
-   if (!file.exists(demofile)) stop(demofile, " not found");
-   dg <- read.csv("demographics.txt");
+    if (!file.exists(demofile)) stop(demofile, " not found")
+    dg <- read.csv("demographics.txt")
 
-   ## extract experiment names from infile paths
-   nexp <- length(infiles)
-   splits <- strsplit(infiles, "/")
-   exp.names <- character(nexp)
-   sublist <- NULL
-   explist <- NULL
-   for (i in 1:nexp) {
-      exp.names[i] <- splits[[i]][3]
-      if (!file.exists(infiles[i])) stop(infiles[i], " not found")
-      load(infiles[i])
-      subs <- unique(as.character(data01$sub))
-      sublist <- c(sublist, subs)
-      explist <- c(explist, rep(i, length(subs)))
-   }
+    ## Extract experiment names from infile paths.
+    nexp <- length(infiles)
+    splits <- strsplit(infiles, "/")
 
-   sink(outfile)
+    ## Extract all subjects and the experiments they ran in.
+    exp.names <- character(nexp)
+    sublist <- NULL
+    explist <- NULL
+    for (i in 1:nexp) {
+        exp.names[i] <- splits[[i]][3]
+        if (!file.exists(infiles[i])) stop(infiles[i], " not found")
+        load(infiles[i])
+        subs <- unique(as.character(data01$sub))
+        sublist <- c(sublist, subs)
+        explist <- c(explist, rep(exp.names[i], length(subs)))
+    }
 
-   subs <- sort(unique(sublist));
-   nsubs <- length(subs);
+    subs <- sort(unique(sublist))
+    nsubs <- length(subs)
+    names(explist) <- sublist
 
-   if (nsubs > dim(dg)[1]) {
-      stop(demofile, " has ", dim(dg)[1], " subjects, but there are ",
-           nsubs, " subjects across all the experiments");
-   }
+    ## Record demographics for all the subjects run in the various experiments,
+    ## along with the experiments in which they participated.
+    dem <- array(NA, dim = c(nsubs, 2 + nexp),
+                 dimnames = list(subs,
+                   c("sex", "age", exp.names)))
+    for (s in subs) {
+        if (s %in% dg$Subject) {
+            dem[s, c("sex")] <- dg[match(s, dg$Subject), c("Gender")]
+            dem[s, c("age")] <- dg[match(s, dg$Subject), c("Age")]
+        }
+        subexp <- explist[names(explist) == s]
+        dem[s, exp.names] <- 0
+        for (x in subexp) {
+            dem[s, x] <- 1
+        }
+    }
 
-   index <- rep(FALSE, dim(dg)[1]);
-   for (sub in subs) {
-      index[dg$Subject == sub] <- TRUE;
-   }
-   dg <- dg[index, ];
-   print(dg);
+    ## Generate report of the experiments each subject participated in, along
+    ## with the total number of experiments per subject.
+    expcount <- array(0, dim = c(nsubs, nexp+1),
+                      dimnames = list(subs, c(exp.names, "count")))
+    expcount[, exp.names] <- dem[, exp.names]
+    expcount[, "count"] <- apply(expcount[, exp.names], 1, sum)
 
-   output <- matrix(0, nrow=nsubs, ncol=nexp+1, dimnames = list(subs, c(exp.names, "count")));
+    ## Demographics summary for each experiment
+    dm <- array(0, dim = c(nexp, 7),
+                dimnames = list(exp.names,
+                  c("ageMean", "ageMin", "ageMax", "ageMissing",
+                    "nFemale", "nMale", "nMissing")))
+    print(dm)
+    for (x in exp.names) {
+        index <- dem[, x] > 0
+        ages <- dem[index, "age"]
+        dm[x, "ageMean"] <- round(mean(ages, na.rm = TRUE), 1)
+        dm[x, "ageMin"] <- min(ages, na.rm = TRUE)
+        dm[x, "ageMax"] <- max(ages, na.rm = TRUE)
+        dm[x, "ageMissing"] <- sum(is.na(ages))
+        genders <- as.character(dem[index, "sex"])
+        dm[x, "nFemale"] <- sum(genders == 1, na.rm = TRUE)
+        dm[x, "nMale"] <- sum(genders == 2, na.rm = TRUE)
+        dm[x, "nMissing"] <- sum(is.na(genders))
+    }
 
-   for (i in 1:nsubs) {
-      output[i, explist[subs[i] == sublist]] <- 1
-      output[i, nexp+1] <- sum(output[i, 1:nexp])
-      output[i, explist[subs[i] == sublist]] <- 11
-   }
+    sink(outfile)
 
-   print(output)
+    print(expcount)
 
-   cat("\n\nAge and gender distributions\n");
-   dm <- matrix(0, nrow = nexp, ncol = 7,
-                dimnames = list(exp.names, c("ageMean", "ageMin", "ageMax", "ageMissing",
-                  "nFemale", "nMale", "nMissing")));
-   for (i in 1:nexp) {
-      index <- output[,i] > 0;
-      ages <- dg[index, "Age"];
-      dm[i, "ageMean"] <- round(mean(ages, na.rm = T), 1);
-      dm[i, "ageMin"] <- min(ages, na.rm = T);
-      dm[i, "ageMax"] <- max(ages, na.rm = T);
-      dm[i, "ageMissing"] <- sum(is.na(ages));
-      genders <- as.character(dg[index, "Gender"]);
-      dm[i, "nFemale"] <- sum(genders == "F");
-      dm[i, "nMale"] <- sum(genders == "M");
-      dm[i, "nMissing"] <- sum(is.na(genders));
-   }
+    cat("\n\nAge and gender distributions\n");
+    print(dm);
 
-   print(dm);
+    cat("\nTotal number of subjects = ", nsubs, "\n", sep="")
+    cat("Total number of experiments = ", nexp, "\n\n", sep="")
 
-   cat("\nTotal number of subjects = ", nsubs, "\n", sep="")
-   cat("Total number of experiments = ", nexp, "\n\n", sep="")
-
-   numcounts <- 5
-   out2 <- matrix(0, nrow=numcounts, ncol=1)
-   rownames(out2) <- 1:numcounts
-   colnames(out2) <- "count"
-   for (i in 1:numcounts) {
-      out2[i,1] <- sum(output[,nexp+1] == i)
-   }
+    numcounts <- 5
+    out2 <- matrix(0, nrow=numcounts, ncol=1)
+    rownames(out2) <- 1:numcounts
+    colnames(out2) <- "count"
+    for (i in 1:numcounts) {
+        out2[i,1] <- sum(expcount[,nexp+1] == i)
+    }
 
 
-   print(out2)
+    print(out2)
 }
 
-do.an01()
-rm(do.an01)
+f.an01()
+rm(f.an01)
