468fb2e99dcb0d32dfe3ba35139ae3da81b652c6
David Fencsik <david.fencsik@gmail.com>
2008-09-11 04:02:29 +0000

    Added functions to plot data from ProbeTrack6b (Experiment 4): pb4a.r plots across tracking loads, and pb4b.r just plots the 4-target condition.

---
+++ probetrackpaper/an/pb4a.r
@@ -0,0 +1,109 @@
+### pb4a.r: generates plot for ProbeTrack6b (Experiment 4) results, separating
+### out by tracking load
+###
+### $LastChangedDate$
+
+f.pb4a <- function () {
+    rtfile <- "../../probetrack6b/data11.rda";
+    dfile <- "../../probetrack6b/data02.rda";
+    outfile <- "pb4a.pdf";
+    thisfile <- "pb4a.r";
+    exit.function <- function () {
+        if (exists("opar")) par(opar);
+        if (any(names(dev.cur()) == c("postscript", "pdf"))) dev.off();
+    }
+    on.exit(exit.function());
+
+    ## hard code error values for RT and d' (SOA main effect)
+    err.rt <- sqrt(1233 / 8) * qt(.975, 21);
+    err.dp <- sqrt(0.2323 / 8) * qt(.975, 21);
+
+    ## hard code error values for RT and d' (Load main effect)
+    ##    err.rt <- sqrt(5641 / 8) * qt(.975, 21);
+    ##    err.dp <- sqrt(0.2768 / 8) * qt(.975, 21);
+
+    ## plotting limits
+    ylim.rt <- c(300, 800);
+    ylim.dp <- c(1, 4);
+    p.ylim.dp <- 1/3;
+    showy.dp <- seq(ylim.dp[1], ylim.dp[2], by = 1);
+    ploty.dp <- (showy.dp - ylim.dp[1]) / diff(ylim.dp) * diff(ylim.rt) * p.ylim.dp + ylim.rt[1];
+    at.ylab.dp <- mean(ploty.dp);
+    err.dp <- err.dp / diff(ylim.dp) * diff(ylim.rt) * p.ylim.dp;
+
+    if (!file.exists(rtfile)) stop("cannot open input file ", rtfile);
+    if (!file.exists(dfile)) stop("cannot open input file ", dfile);
+    if (IsFileUpToDate(outfile, c(rtfile, dfile, thisfile))) {
+        warning("Output file is up to date, no action taken");
+        return(invisible(NULL));
+    }
+
+    ## load data files and rename the datasets
+    load(rtfile);
+    data.rt <- data11$data;
+    model.rt <- data11$model;
+    load(dfile);
+    data.dp <- data02;
+
+    ## filter out unused trials
+    data.dp <- data.dp[data.dp$gapdur != "0", ];
+
+    ## extract data to plot
+    rt <- with(data.rt, tapply(obs, list(soa, ntargets), mean));
+    dp <- (with(data.dp, tapply(dprime, list(soa, ntargets), mean)) - ylim.dp[1]) /
+        diff(ylim.dp) * diff(ylim.rt) * p.ylim.dp + ylim.rt[1];
+    showx <- as.numeric(dimnames(rt)[[1]]);
+    plotx <- showx; plotx[plotx == 1280] <- 500;
+
+    ## compute model for RT data
+    predx <- seq(min(plotx), max(plotx), by = 1);
+    rt.pred <- array(NA, dim = c(length(predx), dim(rt)[[2]]),
+                     dimnames = list(predx, dimnames(rt)[[2]]))
+    rtimes <- c(38.70862, 26.41682, 55.62707, 44.21890)
+    baseRTs <- c(496.9362, 571.3957, 554.9987, 589.8345)
+    names(rtimes) <- names(baseRTs) <- dimnames(rt)[[2]]
+    for (gap in dimnames(rt)[[2]]) {
+        rt.pred[, gap] <- model.rt(predx, rtimes[gap], baseRTs[gap])
+    }
+
+    ## define colors
+    col <- rainbow(dim(rt)[2], v = .75)
+
+    ## open pdf file
+    pdf(outfile, width = 8, height = 6, pointsize = 12);
+    opar <- par(mfrow = c(1, 1), las = 1, pty = "s", cex.axis = .6,
+                mar = c(5, 4, 2, 4), xpd = NA, bg = "white");
+
+    ## prepare plotting area
+    matplot(plotx, rt, type = "n", axes = F,
+            ylim = ylim.rt, xlab = "Probe delay (ms)", ylab = "Averaged median RT (ms)",
+            main = "Experiment 4");
+    axis(1, plotx, showx);
+    axis(2);
+    axis(4, ploty.dp, showy.dp);
+    mtext("d'", side = 4, line = 2, las = 0, at = at.ylab.dp);
+
+    ## plot error bars and points
+    matlines(predx, rt.pred, lwd = 3, col = col, lty = 1);
+    arrows(plotx, rt - err.rt, plotx, rt + err.rt,
+           length = .05, angle = 90, code = 3, lwd = 1, lty = 1,
+           col = rep(col, rep(length(plotx), dim(rt)[[2]])));
+    matpoints(plotx, rt, pch = 21, col = col, bg = "white", lwd = 3, cex = 1.5);
+    arrows(plotx, dp - err.dp, plotx, dp + err.dp,
+           length = .05, angle = 90, code = 3, lwd = 1, lty = 1,
+           col = rep(col, rep(length(plotx), dim(rt)[[2]])));
+    matlines(plotx, dp, type = "o",
+             lwd = 3, lty = 2, col = col, pch = 15, cex = 1.5);
+
+    ## add breaks to x-axis
+    if (require("plotrix")) {
+        axis.break(axis = 1, breakpos = mean(c(80, 500)), style = "slash", brw = 0.02);
+    }
+
+    legend("topright", paste(dimnames(rt)[[2]], "-target(s)", sep=""),
+           bty = "n",
+           col = col, lty = 1, lwd = 2)
+}
+
+f.pb4a();
+rm(f.pb4a);
---
+++ probetrackpaper/an/pb4b.r
@@ -0,0 +1,93 @@
+### pb4b.r: generates plot for ProbeTrack3 results, collapsing across gap
+### duration
+###
+### $LastChangedDate$
+
+f.pb4b <- function () {
+    rtfile <- "../../probetrack6b/data11.rda";
+    dfile <- "../../probetrack6b/data02.rda";
+    outfile <- "pb4b.pdf";
+    thisfile <- "pb4b.r";
+    exit.function <- function () {
+        if (exists("opar")) par(opar);
+        if (any(names(dev.cur()) == c("postscript", "pdf"))) dev.off();
+    }
+    on.exit(exit.function());
+
+    ## hard code error values for RT and d'
+    err.rt <- sqrt(565.3 / 8) * qt(.975, 21);
+    err.dp <- sqrt(0.1508 / 8) * qt(.975, 21);
+
+    ## plotting limits
+    ylim.rt <- c(300, 800);
+    ylim.dp <- c(1, 4);
+    p.ylim.dp <- 1/3;
+    showy.dp <- seq(ylim.dp[1], ylim.dp[2], by = 1);
+    ploty.dp <- (showy.dp - ylim.dp[1]) / diff(ylim.dp) * diff(ylim.rt) * p.ylim.dp + ylim.rt[1];
+    at.ylab.dp <- mean(ploty.dp);
+    err.dp <- err.dp / diff(ylim.dp) * diff(ylim.rt) * p.ylim.dp;
+
+    if (!file.exists(rtfile)) stop("cannot open input file ", rtfile);
+    if (!file.exists(dfile)) stop("cannot open input file ", dfile);
+    if (IsFileUpToDate(outfile, c(rtfile, dfile, thisfile))) {
+        warning("Output file is up to date, no action taken");
+        return(invisible(NULL));
+    }
+
+    ## load data files and rename the datasets
+    load(rtfile);
+    data.rt <- data11$data;
+    fit.rt <- data11$fit;
+    model.rt <- data11$model;
+    load(dfile);
+    data.dp <- data02;
+
+    ## filter out unused trials
+    data.rt <- data.rt[data.rt$ntargets == "4", ]
+    data.rt$ntargets <- factor(data.rt$ntargets)
+    data.dp <- data.dp[data.dp$ntargets == "4" & data.dp$gapdur != "0", ]
+    data.dp$ntargets <- factor(data.dp$ntargets)
+
+    ## extract data to plot
+    rt <- with(data.rt, tapply(obs, list(soa), mean));
+    dp <- (with(data.dp, tapply(dprime, list(soa), mean)) - ylim.dp[1]) /
+        diff(ylim.dp) * diff(ylim.rt) * p.ylim.dp + ylim.rt[1];
+    showx <- as.numeric(dimnames(rt)[[1]]);
+    plotx <- showx; plotx[plotx == 1280] <- 500;
+
+    ## compute model for RT data
+    predx <- seq(min(plotx), max(plotx), by = 1);
+    rt.pred <- model.rt(predx, 44.21890, 589.8345);
+
+    ## open pdf file
+    pdf(outfile, width = 8, height = 6, pointsize = 12);
+    opar <- par(mfrow = c(1, 1), las = 1, pty = "s", cex.axis = .6,
+                mar = c(5, 4, 2, 4), xpd = NA, bg = "white");
+
+    ## prepare plotting area
+    plot(plotx, rt, type = "n", axes = F,
+         ylim = ylim.rt, xlab = "Probe delay (ms)", ylab = "Averaged median RT (ms)",
+         main = "Experiment 4");
+    axis(1, plotx, showx);
+    axis(2);
+    axis(4, ploty.dp, showy.dp);
+    mtext("d'", side = 4, line = 2, las = 0, at = at.ylab.dp);
+
+    ## plot error bars and points
+    lines(predx, rt.pred, lwd = 3, col = 1);
+    arrows(plotx, rt - err.rt, plotx, rt + err.rt,
+           length = .05, angle = 90, code = 3, lwd = 2, col = 1, lty = 1);
+    points(plotx, rt, pch = 21, col = 1, bg = "white", lwd = 3, cex = 1.5);
+    arrows(plotx, dp - err.dp, plotx, dp + err.dp,
+           length = .05, angle = 90, code = 3, lwd = 2, col = 1, lty = 1);
+    lines(plotx, dp, type = "o",
+          lwd = 3, lty = 2, pch = 15, cex = 1.5);
+
+    ## add breaks to x-axis
+    if (require("plotrix")) {
+        axis.break(axis = 1, breakpos = mean(c(80, 500)), style = "slash", brw = 0.02);
+    }
+}
+
+f.pb4b();
+rm(f.pb4b);
