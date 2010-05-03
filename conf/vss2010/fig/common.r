df71f3be6dc352983b13c4b2491276b02a9dc2ce
David Fencsik <david.fencsik@gmail.com>
2010-05-02 22:52:55 -0700

    Add plotting code to VSS2010 abstract
    All currently plot RT and d' in a plot reasonably sized for the poster.

---
+++ conf/vss2010/fig/common.r
@@ -0,0 +1,23 @@
+scaling <- 300
+y.axis.range <- 300
+plotSize <- c(6, 4.5) # inches (x, y)
+fontSize <- 18
+pty <- "m"
+xpd <- NA
+mar <- c(4.0, 4.0, 0.25, 3.0)
+
+lwd.model <- 4
+
+pch.rt <- 21
+lwd.pts <- 3
+cex.pts <- 1.4
+
+lwd.ci <- 3
+
+pch.dp <- 22
+lwd.dp <- 2
+lwd.ci.dp <- 2
+cex.pts.dp <- 1.0
+
+lwd.axis <- 2
+cex.axis <- 0.75
---
+++ conf/vss2010/fig/pb2.r
@@ -0,0 +1,101 @@
+### pb2.r: generates plot for ProbeTrack09 results
+
+do.pb2 <- function () {
+    rtfile <- "../../../probetrack09/data11.rda"
+    dfile <- "../../../probetrack09/data02.rda"
+    outfile <- "pb2.pdf"
+    thisfile <- "pb2.r"
+
+    on.exit(if (exists("opar")) par(opar))
+    on.exit(if (any(names(dev.cur()) == c("postscript", "pdf"))) dev.off(),
+            TRUE)
+
+    source("common.r")
+
+    ## hard code error values for RT and d'
+    err.rt <- sqrt(528.8 / 10) * qt(.975, 27)
+    err.dp <- sqrt(0.1821 / 10) * qt(.975, 27)
+
+    ## plotting limits
+    ylim.max <- 600
+    ylim.rt <- c(ylim.max - y.axis.range, ylim.max);
+    ylim.dp <- c(0, 3)
+    p.ylim.dp <- 1/3
+    showy.dp <- seq(ylim.dp[1], ylim.dp[2], by=1)
+    ploty.dp <- (showy.dp - ylim.dp[1]) / diff(ylim.dp) *
+        diff(ylim.rt) * p.ylim.dp + ylim.rt[1]
+    at.ylab.dp <- mean(ploty.dp)
+    err.dp <- err.dp / diff(ylim.dp) * diff(ylim.rt) * p.ylim.dp
+
+    ## load data files and rename the datasets
+    load(rtfile)
+    data.rt <- data11$data
+    fit.rt <- data11$fit
+    model.rt <- data11$model
+    load(dfile)
+    data.dp <- data02
+
+    ## recode vars and filter out unused trials
+    data.rt$soa <- as.numeric(as.character(data.rt$soa))
+    data.rt <- data.rt[data.rt$gapdur != "0", , drop=TRUE]
+    data.dp$soa <- as.numeric(as.character(data.dp$soa))
+    data.dp <- data.dp[data.dp$gapdur != "0", , drop=TRUE]
+
+    ## extract data to plot
+    rt <- with(data.rt, tapply(rt, list(soa), mean))
+    dp <- (with(data.dp, tapply(dprime, list(soa), mean)) - ylim.dp[1]) /
+        diff(ylim.dp) * diff(ylim.rt) * p.ylim.dp + ylim.rt[1]
+    showx <- as.numeric(dimnames(rt)[[1]])
+    plotx <- showx
+    plotx[plotx == 1280] <- scaling
+
+    ## compute model for RT data
+    Subjects <- sort(unique(as.character(data.rt$sub)))
+    predx <- seq(min(plotx), max(plotx), by=1)
+    rt.pred <- array(dim=c(length(Subjects), length(predx)),
+                     dimnames=list(Subjects, predx))
+    rtime <- fit.rt$rtime
+    baseRT <- fit.rt$baseRT
+    names(baseRT) <- names(rtime) <- fit.rt$sub
+    for (sub in Subjects) {
+        rt.pred[sub, ] <- model.rt(predx, rtime[sub], baseRT[sub])
+    }
+    rt.pred <- apply(rt.pred, 2, mean)
+
+    ## or just compute based on average parameters
+    ##rt.pred <- model.rt(predx, 40.345929, 493.494198)
+
+    ## open pdf file
+    pdf(outfile, width=plotSize[1], height=plotSize[2], pointsize=fontSize)
+    opar <- par(mfrow=c(1, 1), las=1, pty=pty, cex.axis=cex.axis,
+                mar=mar, xpd=xpd, bg="white")
+
+    ## prepare plotting area
+    plot(plotx, rt, type="n", axes=F,
+         ylim=ylim.rt, main="",
+         xlab="Probe delay (ms)", ylab="Averaged median RT (ms)")
+    axis(1, plotx, showx, lwd=lwd.axis, cex.axis=cex.axis)
+    axis(2, seq(ylim.rt[1], ylim.rt[2], by=100),
+         lwd=lwd.axis, cex.axis=cex.axis)
+    axis(4, ploty.dp, showy.dp, lwd=lwd.axis, cex.axis=cex.axis)
+    mtext("d'", side=4, line=2, las=0, at=at.ylab.dp)
+
+    ## plot error bars and points
+    lines(predx, rt.pred, lwd=lwd.model, col=1)
+    arrows(plotx, rt - err.rt, plotx, rt + err.rt,
+           length=.05, angle=90, code=3, lwd=lwd.ci, col=1, lty=1)
+    points(plotx, rt, pch=pch.rt, col=1, bg="white", lwd=lwd.pts, cex=cex.pts)
+    arrows(plotx, dp - err.dp, plotx, dp + err.dp,
+           length=.05, angle=90, code=3, lwd=lwd.ci.dp, col=1, lty=1)
+    lines(plotx, dp, type="o", bg="white",
+          lwd=lwd.dp, lty=2, pch=pch.dp, cex=cex.pts.dp)
+
+    ## add breaks to x-axis
+    if (require("plotrix")) {
+        axis.break(axis=1, breakpos=mean(c(160, scaling)),
+                   style="slash", brw=0.02)
+    }
+}
+
+do.pb2()
+rm(do.pb2)
