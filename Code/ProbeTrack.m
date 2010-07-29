9d8194230c4dd9714d17684bc7d1344a84295eab
David Fencsik <david.fencsik@gmail.com>
2010-07-29 12:21:02 -0700

    Advance minor version number

--- Code/ProbeTrack.m
+++ Code/ProbeTrack.m
@@ -2,7 +2,7 @@ function ProbeTrack
 
 % Runs MOT task with gap and variable post-gap probe-onset delay
 
-    VERSION = '11.0';
+    VERSION = '11.1';
     try
         AssertOpenGL;
         InitializePsychSound;
