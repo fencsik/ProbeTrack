fb3c2b5a37b5ab6227d228db6ffafed4cd374973
David Fencsik <david.fencsik@gmail.com>
2012-02-08 13:41:41 -0800

    Add basic analysis files to ProbeTrack15.
    These include all those currently in ProbeTrack13.  Modify the plots to
    display ProbeTrack15 in the title.

---
+++ probetrack15/data00.r
@@ -0,0 +1,34 @@
+### data00: cleans up raw data file
+
+f.data00 <- function () {
+    infile <- "rawdata.rda"
+    outfile <- "data00.rda"
+
+    load(infile)
+    data00 <- alldata
+
+### remove bad subjects (none so far):
+
+### remove practice blocks, bad keypresses, and RTs <= 0
+    data00 <- data00[data00$prac == 0 & data00$acc >= 0 & data00$rt > 0, ]
+    data00$blocktype <- factor(data00$blocktype)
+    data00$resp <- factor(data00$resp)
+
+### rename and recode variables
+    ## probeType == 1 -> target; probeType == 2 -> distractor
+    data00$target <- factor(data00$probeTarget, levels = c(0, 1),
+                            labels = c("distractor", "target"))
+
+    ## clean-up soa and convert to factor
+    data00$soa <- round(data00$soa * 1000 / 75) # convert to ms
+    data00$soa <- factor(data00$soa, levels = sort(unique(data00$soa)))
+
+    ## clean up gap type
+    data00$gaptype <- factor(as.character(data00$gaptype),
+                             levels=c("Blank", "SmallFlash", "BigFlash"))
+
+    save(data00, file=outfile)
+}
+
+f.data00()
+rm(f.data00)
---
+++ probetrack15/fig0201.r
@@ -0,0 +1,47 @@
+### fig0201.r: plot d' by probe delay and condition
+
+f.fig0201 <- function () {
+    infile <- "data02.rda"
+    outfile <- "fig0201.pdf"
+    thisfile <- "fig0201.r"
+    exit.function <- function () {
+        if (exists("opar")) par(opar)
+        if (any(names(dev.cur()) == c("postscript", "pdf"))) dev.off()
+    }
+    on.exit(exit.function())
+
+    load(infile)
+    data02$soa <- as.numeric(as.character(data02$soa))
+
+    ## extract relevant data
+    dt <- with(data02, tapply(dprime, list(soa, cond), mean))
+    x <- as.numeric(dimnames(dt)[[1]])
+    col <- rainbow(length(dimnames(dt)[[2]]))
+
+    ## CIs based on MSE from SOA main effect for each cond
+    ## order is Blank, SmallFlash, BigFlash
+    errg <- sqrt(c(.21214, .16953, .183773) / 17) * qt(.975, 48)
+
+    pdf(outfile, width=6, height=6, pointsize=12)
+    opar <- par(mfrow=c(1, 1), las=1, pty="m", cex.axis=.6,
+                xpd=NA, bg="white")
+
+    matplot(x, dt, type="n", bty="n",
+            ylim=c(0, 4), axes=F,
+            xlab="Probe delay (ms)", ylab="d'", main="ProbeTrack15")
+    axis(1, x)
+    axis(2)
+
+    for (i in 1:length(col)) {
+        arrows(x, dt[, i] - errg[i], x, dt[, i] + errg[i],
+               length=.05, angle=90, code=3, col=col[i], lwd=2, lty=1)
+    }
+    matlines(x, dt, type="o",
+          col=col, pch=21, lty=1, lwd=2, cex=1.5, bg="white")
+    legend("bottomright", dimnames(dt)[[2]],
+           bty="n", cex=.75, pt.cex=1.2,
+           pt.bg="white", col=col, pch=21, lty=1, lwd=2)
+}
+
+f.fig0201()
+rm(f.fig0201)
