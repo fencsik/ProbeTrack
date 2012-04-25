b72eec77b2e4cef303bdd8ad6045dda5aaa7260b
David Fencsik <david.fencsik@gmail.com>
2012-04-25 15:30:13 -0700

    Switch error bars to gapdur effect.
    Instead of having separate bars for each probe delay, just use the one for
    the gap duration effect.  Remove all references to the error file.

--- probetrack03/fig1001.r
+++ probetrack03/fig1001.r
@@ -6,7 +6,6 @@ do.fig1001 <- function () {
    infile <- "data10.rda";
    outfile <- "fig1001.pdf";
    thisfile <- "fig1001.r";
-   errfile <- "an1002.rda";
    exit.function <- function () {
       if (exists("opar")) par(opar);
       if (any(names(dev.cur()) == c("postscript", "pdf"))) dev.off();
@@ -14,7 +13,7 @@ do.fig1001 <- function () {
    on.exit(exit.function());
 
    if (!file.exists(infile)) stop("cannot open input file ", infile);
-   if (IsFileUpToDate(outfile, c(infile, thisfile, errfile))) {
+   if (IsFileUpToDate(outfile, c(infile, thisfile))) {
       warning("Output file is up to date, no action taken");
       return(invisible(NULL));
    }
@@ -25,12 +24,9 @@ do.fig1001 <- function () {
    ## extract relevant data
    dtg <- with(data10[data10$gapdur != "0", ],
                tapply(rt, list(soa, gapdur), mean, na.rm = TRUE));
-   if (file.exists(errfile)) {
-      load(errfile);
-      errg <- an1002[, , "ci"];
-   } else {
-      errg <- NULL;
-   }
+   errg <- sqrt(c(1221, 1084, 1082) / 8) * qt(.975, 49)
+   names(errg) <- c("133", "307", "507")
+
    x <- as.numeric(dimnames(dtg)[[1]]);
    gapdurList <- dimnames(dtg)[[2]];
    dtng <- rep(mean(data10[data10$gapdur == "0", "rt"], na.rm = F), length(x));
@@ -53,7 +49,7 @@ do.fig1001 <- function () {
          col = 1, lty = 2, lwd = 3);
    for (gd in gapdurList) {
       if (!is.null(errg)) {
-         arrows(x, dtg[, gd] - errg[, gd], x, dtg[, gd] + errg[, gd],
+         arrows(x, dtg[, gd] - errg[gd], x, dtg[, gd] + errg[gd],
                 length = .05, angle = 90, code = 3, lwd = 2, col = col[gd], lty = 1);
       }
       lines(x, dtg[, gd], type = "o",
