7950a0ab5c3b35ff44a5cf5627899aeaf11f4c44
David Fencsik <david.fencsik@gmail.com>
2012-06-04 22:32:45 -0700

    Add output about # of subjects and trials dropped.

--- probetrack15/data00.r
+++ probetrack15/data00.r
@@ -8,6 +8,7 @@ f.data00 <- function () {
     data00 <- alldata
 
 ### remove bad subjects:
+    sub.pre <- length(unique(data00$sub))
     ## Subject 1 had 322 early responses out of 576 trials
     data00 <- data00[data00$sub != 1, ]
     ## Subject 12 had 95
@@ -16,15 +17,20 @@ f.data00 <- function () {
     ##data00 <- data00[data00$sub != 7, ]
     ## Subject 14 ran out of time and couldn't finish
     data00 <- data00[data00$sub != 14, ]
+    cat(sprintf("Dropped %0.0f of %0.0f subject(s)\n",
+                sub.pre - length(unique(data00$sub)), sub.pre))
 
 ### remove practice blocks, bad keypresses, and responses that are too slow
 ### or too fast; note that there are some extremely large RTs, which are
 ### actually just early responses that were mishandled by the code: they
 ### were set to the clock rather than the RT
+    trials.pre <- sum(data00$prac == 0)
     data00 <- data00[data00$prac == 0 & data00$acc >= 0 &
                      data00$rt > 50 & data00$rt < 5000, ]
     data00$blocktype <- factor(data00$blocktype)
     data00$resp <- factor(data00$resp)
+    cat(sprintf("Dropped %0.0f of %0.0f experimental trials\n",
+                trials.pre - sum(data00$prac == 0), trials.pre))
 
 ### rename and recode variables
     ## probeType == 1 -> target; probeType == 2 -> distractor
