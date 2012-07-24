a88cc3c0707ff5987adcdfece6aff588af82dd4a
David Fencsik <david.fencsik@gmail.com>
2012-07-24 16:45:59 -0700

    Fit model to RT collapsed across gap duration.
    Add scripts to fit the model and generate tables and figures.

---
+++ probetrack03/data14.r
@@ -0,0 +1,76 @@
+### data14.r: fit reacquire-time model to RT by probe delay functions from
+### data13
+
+do.data14 <- function () {
+   thisfile <- "data14.r"
+   infile <- "data13.rda"
+   outfile <- "data14.rda"
+
+   exit.function <- function () {
+      if (exists("old.opt")) options(old.opt)
+   }
+   on.exit(exit.function())
+   old.opt <- options(warn=1)
+
+   if (!file.exists(infile)) stop("cannot open input file ", infile)
+   if (IsFileUpToDate(outfile, c(thisfile, infile))) {
+      warning("Output file is up to date, no action taken")
+      return(invisible(NULL))
+   }
+   load(infile)
+
+   ## set-up data matrix
+   data13$sub <- as.character(data13$sub)
+   data13$soa <- as.numeric(as.character(data13$soa))
+   data <- with(data13, aggregate(data.frame(rt=rt),
+                                  list(soa=soa, sub=sub), mean))
+   data$soa <- as.numeric(as.character(data$soa))
+   data$rt.pred <- numeric(length(data$rt))
+   Subjects <- as.character(sort(unique(data$sub)))
+
+   ## set-up matrix for storing fit information
+   fit <- matrix(NA, nrow=length(Subjects), ncol=8,
+                 dimnames=list(Subjects,
+                   c("rtime", "baseRT", "iter", "code",
+                     "sse", "rmse", "r", "chisq")))
+   fit.columns <- 1:ncol(fit)
+
+   reacquire.model <- function (soa, rtime, baseRT) {
+      baseRT + ifelse(rtime - soa > 0, rtime - soa, 0)
+   }
+
+   GoodnessOfFit <- function(p) {
+      ## parameters: (1) rtime, (2) baseRT
+###      if (p[1] > 0 && p[2] > 0 && abs(p[3] - min(y)) < min(y) / 2) {
+      sum( (y - reacquire.model(x, rtime=p[1], baseRT=p[2])) ^ 2 )
+###      } else {
+###         10^12
+###      }
+   }
+
+   p0 <- c(40, 500)
+   names(p0) <- c("rtime", "baseRT")
+
+   for (sub in Subjects) {
+      data.index <- data$sub == sub
+      fit.index <- rownames(fit) == sub
+      x <- data[data.index, "soa"]
+      y <- data[data.index, "rt"]
+      p0["baseRT"] <- min(y)
+      out <- nlm(GoodnessOfFit, p0, print.level=0)
+      p <- out$estimate
+      yhat <- reacquire.model(x, p[1], p[2])
+      data[data.index, "rt.pred"] <- yhat
+      fit[fit.index, fit.columns] <- c(p, out$iterations, out$code,
+                                       GoodnessOfFit(p),
+                                       sqrt(mean((y - yhat)^2)),
+                                       cor(y, yhat),
+                                       sum((y - yhat) ^ 2 / yhat))
+   }
+
+   data14 <- list(data=data, fit=data.frame(fit), model=reacquire.model)
+   save(data14, file=outfile)
+}
+
+print(system.time(do.data14(), TRUE))
+rm(do.data14)
---
+++ probetrack03/fig1402.r
@@ -0,0 +1,65 @@
+### fig1402.r: plot fit of model against observed data for each subject
+
+do.fig1402 <- function () {
+   infile <- "data14.rda"
+   outfile <- "fig1402.pdf"
+   thisfile <- "fig1402.r"
+   exit.function <- function () {
+      if (exists("opar")) par(opar)
+      if (any(names(dev.cur()) == c("postscript", "pdf"))) dev.off()
+   }
+   on.exit(exit.function())
+
+   if (!file.exists(infile)) stop("cannot open input file ", infile)
+   if (IsFileUpToDate(outfile, c(infile, thisfile))) {
+      warning("Output file is up to date, no action taken")
+      return(invisible(NULL))
+   }
+   load(infile)
+   model <- data14$model
+   fit <- data14$fit
+   dt <- with(data14$data, tapply(rt, list(soa, sub), mean))
+
+   x <- as.numeric(dimnames(dt)[[1]])
+   subList <- dimnames(dt)[[2]]
+
+   pdf(outfile, width=18, height=8, pointsize=14)
+   opar <- par(mfrow=c(2, 4), las=1, pty="m", cex.axis=.6,
+               xpd=NA, bg="white")
+
+##    ## set up color matrix
+##    col <- as.character(with(data14$data, tapply(rt, list(gapdur, ntargets), mean)))
+##    col <- matrix(rainbow(length(gapdurList) * length(ntargetsList)),
+##                  nrow=length(gapdurList), ncol=length(ntargetsList),
+##                  dimnames=list(gapdurList, ntargetsList))
+   ## other settings
+   ylim.range <- 300
+
+   counter <- 0
+   for (sub in subList) {
+      ylim <- c((mid <- mean(dt[, sub])) - ylim.range/2, mid + ylim.range/2)
+      plot(x, dt[, sub], type="n", bty="n",
+           axes=F, ylim=ylim,
+           xlab="", ylab="", main=paste("ProbeTrack3", sub))
+      axis(1, x)
+      axis(2)
+      if (counter %% 8 >= 3) {
+         title(xlab="Probe delay (ms)")
+      }
+      if (counter %% 4 == 0) {
+         title(ylab="Probe RT (ms)")
+      }
+      index <- rownames(fit) == sub
+      lines(i <- seq(min(x), max(x), by=.1),
+            model(i, fit[index, "rtime"], fit[index, "baseRT"]),
+            lty=1, lwd=3, col=1)
+      abline(h=fit[index, "baseRT"], xpd=F,
+             lty=2, col=1, lwd=2)
+      points(x, dt[, sub],
+             col=1, bg="white", pch=21, cex=1.5, lwd=3)
+      counter <- counter + 1
+   }
+}
+
+do.fig1402()
+rm(do.fig1402)
